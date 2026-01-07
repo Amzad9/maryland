520409e3497817a64e0aabe04c463cca56b57761
Amzad Khan <weblib@192.168.1.2>
2026-01-07 20:41:49 +0530

    add coming soon

---
+++ maryland/app/solution/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SolutionPage = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-8xl font-bold'>Coming Soon...</h1>
+    </div>
+  )
+}
+
+export default SolutionPage
