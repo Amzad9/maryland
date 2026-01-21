b6a6b4133ba95b2f11cdc39da2d7ed4e38bb3145
Amzad Khan <weblib@192.168.1.4>
2026-01-21 21:47:00 +0530

    ios page design

--- maryland/app/getquote/page.tsx
+++ maryland/app/getquote/page.tsx
@@ -46,7 +46,7 @@ export default function QuotePage() {
                     {/* LEFT – IMAGE */}
                     <div className="relative hidden md:block">
                         <img
-                            src="https://plus.unsplash.com/premium_photo-1658506660851-63aa98e86cf8?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            src="https://images.unsplash.com/photo-1689799514696-b16af9b53753?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Business growth"
                             className="h-full w-full object-cover"
                         />
